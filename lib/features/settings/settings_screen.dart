31ca6497178d337aed743eb6e32c3e549f528066
unknown <hnaa12@daum.net>
2024-05-07 22:06:25 +0900

    13 Settings 13.2 Pickers

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -8,42 +6,77 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CupertinoActivityIndicator(),
-          const CircularProgressIndicator(),
-          const CircularProgressIndicator
-              .adaptive(), // ios에서는  ios인디케이터, android에선 android인디케이터 보여줌
-          SizedBox(
-            height: 500,
-            child: ListWheelScrollView(
-              itemExtent: 100, // 리스트 안의 아이템 높이를 지정
-              children: [
-                for (var x in [1, 2, 3, 4, 5, 6, 7, 8, 9])
-                  FractionallySizedBox(
-                    widthFactor: 1,
-                    child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.amber,
-                      child: const Text(
-                        "pick!",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: Sizes.size36,
+        appBar: AppBar(
+          title: const Text("Settings"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              onTap: () async {
+                final date = await showDatePicker(
+                  builder: (context, child) {
+                    return Theme(
+                      data: Theme.of(context).copyWith(
+                        colorScheme: const ColorScheme.light(
+                          primary: Colors.greenAccent,
+                          onPrimary: Colors.black,
+                          onSurface: Colors.black,
+                        ),
+                        textButtonTheme: TextButtonThemeData(
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-              ],
+                      child: child!,
+                    );
+                  },
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1984),
+                  lastDate: DateTime(2034),
+                );
+                print(date);
+
+                if (context.mounted) {
+                  final time = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  print(time);
+                }
+
+                if (context.mounted) {
+                  final booking = await showDateRangePicker(
+                    context: context,
+                    firstDate: DateTime(1984),
+                    lastDate: DateTime(2034),
+                    builder: (context, child) {
+                      return Theme(
+                        data: Theme.of(context).copyWith(
+                          colorScheme: const ColorScheme.light(
+                            primary: Colors.greenAccent,
+                            onPrimary: Colors.black,
+                            onSurface: Colors.black,
+                          ),
+                          textButtonTheme: TextButtonThemeData(
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.black,
+                            ),
+                          ),
+                        ),
+                        child: child!,
+                      );
+                    },
+                  );
+                  print(booking);
+                }
+              },
+              title: const Text("What is your birthday?"),
             ),
-          ),
-        ],
-      ),
-    );
+            const AboutListTile(),
+          ],
+        ));
   }
 }
