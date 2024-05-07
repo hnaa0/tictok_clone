5f14bdb3cc71ea21004c09ba7495d36855587e74
unknown <hnaa12@daum.net>
2024-05-07 22:36:25 +0900

    13 Settings 13.4 Alertdialogs

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -28,37 +30,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
         body: ListView(
           children: [
             // ios = ios 스타일, android = android 스타일 보여줌
-            Switch.adaptive(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            CupertinoSwitch(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            Switch(
-              activeColor: Colors.pinkAccent,
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            SwitchListTile(
-              activeColor: Colors.greenAccent,
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-              title: const Text("Switch List Tile"),
-            ),
-            // ios = ios 스타일, android = android 스타일 보여줌
             SwitchListTile.adaptive(
               activeColor: Colors.greenAccent,
               value: _notifications,
               onChanged: _onNotificationsChanged,
-              title: const Text("Switch List Tile"),
+              title: const Text("Enable notifications"),
+              subtitle: const Text("blabla"),
             ),
-            Checkbox(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            CheckboxListTile(
+            CheckboxListTile.adaptive(
               activeColor: Theme.of(context).primaryColor,
               value: _notifications,
               onChanged: _onNotificationsChanged,
@@ -128,6 +107,57 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 }
               },
               title: const Text("What is your birthday?"),
+            ),
+            ListTile(
+              onTap: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    title: const Text("Are you sure?"),
+                    content: const Text("Please don't go T.T"),
+                    actions: [
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("No"),
+                      ),
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        isDestructiveAction: true,
+                        child: const Text("Yes"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              title: const Text(
+                "Log out(IOS)",
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text("Are you sure?"),
+                    content: const Text("Please don't go T.T"),
+                    actions: [
+                      IconButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        icon: const FaIcon(FontAwesomeIcons.x),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("Yes"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              title: const Text(
+                "Log out(Adroid)",
+                style: TextStyle(color: Colors.red),
+              ),
             ),
             const AboutListTile(),
           ],
